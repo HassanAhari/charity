5693b6ef4c4370f9d6deaf4e28d0a0d1c6d1100e
ahari <hseoleimaneahary@gmail.com>
2021-02-23 00:21:28 +0330

    create account

--- accounts/views.py
+++ accounts/views.py
@@ -5,11 +5,14 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .serializers import UserSerializer
+from accounts.models import User
 
 
 class LogoutAPIView(APIView):
     pass
 
 
-class UserRegistration(APIView):
-    pass
+class UserRegistration(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny, )
